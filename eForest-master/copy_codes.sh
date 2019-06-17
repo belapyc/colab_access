7dfe727b9a40e288614d4a1c0b1c2d9fe8b70a56
belapyc <artuom.gro3@gmail.com>
2019-06-17 13:38:04 +0100

    Removed for_finance and eForest from master branch.(they still on decisionTree branch)

--- eForest-master/copy_codes.sh
+++
@@ -1,4 +0,0 @@
-cp eforest/forest.py scikit-learn/sklearn/ensemble/
-cp eforest/_tree.pxd scikit-learn/sklearn/tree/
-cp eforest/_tree.pyx scikit-learn/sklearn/tree/
-cp eforest/tree.py scikit-learn/sklearn/tree/

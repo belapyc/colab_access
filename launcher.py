b2fe21552aeea69a120672642c0a8935dbf6b8d2
belapyc <artuom.gro3@gmail.com>
2019-06-20 17:14:43 +0100

    Updated

--- launcher.py
+++ launcher.py
@@ -7,6 +7,7 @@ import for_finance
 from data_prep import DataPrep
 from auto_encoder import SAE_train,SAE_predict
 import LSTM
+from __init__ import PARAMETERS, FILE_ADDRESS
 
 '''
 
@@ -16,7 +17,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", dest="data_path", default=FILE_ADDRESS,
                     help='Path to the data.')
-    parser.add_argument("--curr_year", dest="curr_year", type=int, default=CURRENT_YEAR,
+    parser.add_argument("--curr_year", dest="curr_year", type=int, default=PARAMETERS['CURRENT_YEAR'],
                     help='For which year to perform predicting.')
     parser.add_argument("--verbose", dest="verbose", type=int, default=30,
                     help='Set a level of verbosity')
@@ -39,17 +40,16 @@ if __name__ == "__main__":
     data_preparer.read_file(FILE_ADDRESS)
     data_preparer.initial_prep()
 
-    encoders, decoders = SAE_train(data_preparer.data, HIDDEN_LAYERS_AUTOENCODER, EPOCHS, BATCH_SIZE_AUTOENCODER, DEPTH_SAE, SHOW_PROGRESS)
+    encoders, decoders = SAE_train(data_preparer.data, PARAMETERS['HIDDEN_LAYERS_AUTOENCODER'], PARAMETERS['EPOCHS'], PARAMETERS['BATCH_SIZE_AUTOENCODER'], PARAMETERS['DEPTH_SAE'], PARAMETERS['SHOW_PROGRESS'])
     features = SAE_predict(encoders, decoders, data_preparer.data)
     print("Features predicted")
     features = pd.DataFrame(features)
     features = features.loc[:, (features != 0).any(axis=0)]
-    INPUT_SHAPE += features.shape[1]
-    print('New input shape: ', INPUT_SHAPE)
-    PARAMETERS = [SPLIT_PERIOD, HIDDEN_LSTM_UNITS, TEST_TRAIN_SPLIT_COEFFICENT, CURRENT_YEAR, EPOCHS, BATCH_SIZE, INPUT_SHAPE, SHOW_PROGRESS]
+    # INPUT_SHAPE += features.shape[1]
+    # print('New input shape: ', INPUT_SHAPE)
+
 
     data_preparer.data_preparing(features)
 
-    data_preparer.choose_year(CURRENT_YEAR)
 
-    LSTM.run_algorithm(data_preparer, CURRENT_YEAR, SPLIT_PERIOD, TEST_TRAIN_SPLIT_COEFFICENT, PARAMETERS)
+    LSTM.run_algorithm(data_preparer, PARAMETERS['CURRENT_YEAR'], PARAMETERS['SPLIT_PERIOD'], PARAMETERS['TEST_TRAIN_SPLIT_COEFFICENT'], [PARAMETERS[key] for key in PARAMETERS])

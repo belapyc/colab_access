8b079e2244f357d1a89c3f0316500dcdca0f5416
belapyc <artuom.gro3@gmail.com>
2019-07-24 17:26:53 +0100

    Added info to file output.

--- launcher.py
+++ launcher.py
@@ -109,6 +109,7 @@ if __name__ == "__main__":
     f.write("Predicted based on "+ PARAMETERS['INPUT_SHAPE'], " features")
     f.write("wavelet: "+ str(args.wavelet) + "\n")
     f.write("time: " + str(elapsed_time))
+    f.write(str(PARAMETERS))
     f.write("Total profitability: ", str(total_profits))
 
     f.write(str(profits_per_year))

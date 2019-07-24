974f9d7ffe3ef45c3eca68fa49cb5a4b65e5d56d
belapyc <artuom.gro3@gmail.com>
2019-07-24 17:54:52 +0100

    Deleted unnecessary files.

--- copy_codes.sh
+++
@@ -1,4 +0,0 @@
-cp eForest-master/eforest/forest.py scikit-learn/sklearn/ensemble/
-cp eForest-master/eforest/_tree.pxd scikit-learn/sklearn/tree/
-cp eForest-master/eforest/_tree.pyx scikit-learn/sklearn/tree/
-cp eForest-master/eforest/tree.py scikit-learn/sklearn/tree/

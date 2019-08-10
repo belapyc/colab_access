6808989f7b94cc5cc03c305606b0b066e527a342
Artsiom Hramyka <artsiomhramyka@Artsioms-MacBook-Pro.local>
2019-08-10 15:10:29 +0100

    Restructuring of repo, results

--- plotting.py
+++ plotting.py
@@ -3,32 +3,51 @@ import numpy as np
 import pandas as pd
 from data_prep import DataPrep
 from __init__ import PARAMETERS, FILE_ADDRESS
+# , "APPL" "RL_1":"RL","PM-1":"PM-1","NVID":"Nvidia",  "NQ10":"NQ10",
+instruments= { "NFLX":"NFLX",  "APPL":"Apple","eure":"eure"}
 
-preds_forest = pd.read_csv("predictions/forest/Apple/APPL_forest_True_wavelet_True_randomSeed_0_forest_no_10_YEAR_2009predictions.csv")
-preds_sae = pd.read_csv("predictions/SAE/APPL_forest_False_wavelet_True_randomSeed_0_forest_no_10_YEAR_2009predictions")
-actual = pd.read_csv("actual_values/APPL_YEAR_2009actual.csv")
+for instrument in instruments:
+    print(instrument)
+    print(instruments[instrument])
+    for year in PARAMETERS['ALL_YEARS']:
+        preds_forest = pd.read_csv("predictions/forest/rn5f5/"+instrument+"_forest_True_wavelet_True_randomSeed_5_forest_no_5_YEAR_"+str(year)+"predictions")
+        preds_sae = pd.read_csv("predictions/SAE/rn5/"+instrument+"_forest_False_wavelet_True_randomSeed_5_forest_no_10_YEAR_"+str(year)+"predictions")
+        actual = pd.read_csv("actual_values/"+instrument+"_YEAR_"+str(year)+"actual.csv")
+        with plt.style.context('dark_background'):
+            plt.figure()
+            plt.plot(preds_forest.to_numpy()[:,0],preds_forest.to_numpy()[:,1],'b--', label="predictions forest", linewidth=1)
+            plt.plot(preds_sae.to_numpy()[:,0],preds_sae.to_numpy()[:,1],'r--', label="predictions sae", linewidth=1)
+            plt.xlabel("Tick")
+            plt.ylabel("Price")
+            plt.plot( actual.to_numpy()[:,0],actual.to_numpy()[:,1], label="actual", linewidth=1)
+            plt.title("Performance of year "+str(year))
+            plt.legend()
+        save_plot_address = "Plots/both/rn5_f5/"+instrument+"_randomSeed_5"+"_forest_no_5_YEAR_"+str(year)+".png"
+        plt.savefig(save_plot_address, bbox_inches='tight', dpi = 300)
+        plt.close()
+        # plt.show()
 
-
-for year in PARAMETERS['ALL_YEARS']:
-    preds_forest = pd.read_csv("predictions/forest/Apple/APPL_forest_True_wavelet_True_randomSeed_0_forest_no_10_YEAR_"+str(year)+"predictions.csv")
-    preds_sae = pd.read_csv("predictions/SAE/APPL_forest_False_wavelet_True_randomSeed_0_forest_no_10_YEAR_"+str(year)+"predictions")
-    actual = pd.read_csv("actual_values/APPL_YEAR_"+str(year)+"actual.csv")
-    with plt.style.context('dark_background'):
-        plt.figure()
-        plt.plot(preds_forest.to_numpy()[:,0],preds_forest.to_numpy()[:,1],'b--', label="predictions forest", linewidth=1)
-        plt.plot(preds_sae.to_numpy()[:,0],preds_sae.to_numpy()[:,1],'r--', label="predictions sae", linewidth=1)
-        plt.xlabel("Tick")
-        plt.ylabel("Price")
-        plt.plot( actual.to_numpy()[:,0],actual.to_numpy()[:,1], label="actual", linewidth=1)
-        plt.title("Performance of year 2009")
-        plt.legend()
-    plt.show()
-
-
-print(preds_forest.shape)
-print(preds_sae.shape)
-print(actual.shape)
-print(preds_forest.head(5))
+# year = 2009
+# preds_forest = pd.read_csv("predictions/forest/"+instruments["RL_1"]+"/"+"RL_1"+"_forest_True_wavelet_True_randomSeed_0_forest_no_10_YEAR_"+str(year)+"predictions")
+# preds_sae = pd.read_csv("predictions/SAE/"+"RL_1"+"_forest_False_wavelet_True_randomSeed_0_forest_no_10_YEAR_"+str(year)+"predictions")
+# actual = pd.read_csv("actual_values/"+"RL_1"+"_YEAR_"+str(year)+"actual.csv")
+#
+# with plt.style.context('dark_background'):
+#     plt.figure()
+#     plt.plot(preds_forest.to_numpy()[:,0],preds_forest.to_numpy()[:,1],'b--', label="predictions forest", linewidth=1)
+#     plt.plot(preds_sae.to_numpy()[:,0],preds_sae.to_numpy()[:,1],'r--', label="predictions sae", linewidth=1)
+#     plt.xlabel("Tick")
+#     plt.ylabel("Price")
+#     plt.plot( actual.to_numpy()[:,0],actual.to_numpy()[:,1], label="actual", linewidth=1)
+#     plt.title("Performance of year 2009")
+#     plt.legend()
+#     save_plot_address = "Plots/both/"+"RL_1"+"_randomSeed_0"+"_forest_no_10_YEAR_"+str(year)+".png"
+#     plt.savefig(save_plot_address, bbox_inches='tight', dpi = 300)
+#
+# print(preds_forest.shape)
+# print(preds_sae.shape)
+# print(actual.shape)
+# print(preds_forest.head(5))
 print(preds_forest.to_numpy()[:,1])
 
 

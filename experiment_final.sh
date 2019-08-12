34d1b7aa29dedf3b8ee819d7bc42113ead4e98a7
Artsiom Hramyka <artsiomhramyka@Artsioms-MacBook-Pro.local>
2019-08-12 14:14:58 +0100

    Changed experiment

--- experiment_final.sh
+++ experiment_final.sh
@@ -34,13 +34,17 @@
 # python launcher.py --wavelet --forest --forest_no=10 --random_seed=5 --data_path="Data/TA/RL_15mins-2009-2018_TA.csv"
 # python launcher.py --wavelet --forest --forest_no=10 --random_seed=5
 # # SAE r 1
-python launcher.py --wavelet --random_seed=1  --data_path="Data/TA/APPLE_15mins_2009-2018_TA.csv"
-#python launcher.py --wavelet  --data_path="Data/TA/DJworld_indust_15mins_2009-2018_TA.csv"
-#python launcher.py --wavelet  --data_path="Data/TA/EUoil_gas_15mins_2009-2018_TA.csv"
-#python launcher.py --wavelet  --random_seed=5 --data_path="Data/TA/eurex_fut_15mins_2009_2018_TA.csv"
-python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/NFLX_15min_2011-2018_TA.csv"
-#python launcher.py --wavelet  --random_seed=5 --data_path="Data/TA/NQ100fut_15mins_2009_2018_TA.csv"
-python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/NVIDIA_15mins_2009-2018_TA.csv"
-python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/PM-15mins-2009_2018_TA.csv"
-python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/RL_15mins-2009-2018_TA.csv"
-python launcher.py --wavelet  --random_seed=1
+# python launcher.py --wavelet --random_seed=1  --data_path="Data/TA/APPLE_15mins_2009-2018_TA.csv"
+# #python launcher.py --wavelet  --data_path="Data/TA/DJworld_indust_15mins_2009-2018_TA.csv"
+# #python launcher.py --wavelet  --data_path="Data/TA/EUoil_gas_15mins_2009-2018_TA.csv"
+# #python launcher.py --wavelet  --random_seed=5 --data_path="Data/TA/eurex_fut_15mins_2009_2018_TA.csv"
+# python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/NFLX_15min_2011-2018_TA.csv"
+# #python launcher.py --wavelet  --random_seed=5 --data_path="Data/TA/NQ100fut_15mins_2009_2018_TA.csv"
+# python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/NVIDIA_15mins_2009-2018_TA.csv"
+# python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/PM-15mins-2009_2018_TA.csv"
+# python launcher.py --wavelet  --random_seed=1 --data_path="Data/TA/RL_15mins-2009-2018_TA.csv"
+# python launcher.py --wavelet  --random_seed=1
+
+python launcher.py --wavelet --forest --forest_no=5 --random_seed=1
+python launcher.py --wavelet --forest --forest_no=5 --random_seed=0
+python launcher.py --wavelet --forest --forest_no=5 --random_seed=5
